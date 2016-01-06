99702b515184036716d21edf897bbf704d94710c
Varun Kumar <varun.invent@gmail.com>
2016-01-06 17:17:03 +0530

    trying to read files

--- img_read.lua
+++ img_read.lua
@@ -4,7 +4,7 @@
 -------------------------------surveillance_data/two_wheeler
 -- Resize it to 30 X 30
 -- Normalize them feature wise (optional)
--- Save the data and normalization params (mean,std)
+-- Save the data and normalization params (mean,std) as vehicle_3_class_data
 
 
 require 'nn'
@@ -12,87 +12,83 @@ require 'image'
 require 'torch'
 
 
-x1 = 100
-y1 = 180
-x2 = 1200
-y2 = 370
-imgCols = x2-x1
-imgRows = y2-y1
-maxImages = 190 -- Number of image pairs to work on
 
-train_data_path_l = 'training/colored_0/'
-train_data_path_r = 'training/colored_1/'
-gt_train_path = 'training/disp_noc/'
+imgCols =  30
+imgRows = 30
+nImages =  3000 --1000 per class
+nImagePlanes = 3 --RGB
 
 
 
-train_images_list_l = paths.dir(train_data_path_l)
-train_images_list_r = paths.dir(train_data_path_r)
-gt_train_images_list = paths.dir(gt_train_path)
+data_path = {
+'surveillance_data/vehicle_data/car/',
+'surveillance_data/vehicle_data/two_wheeler/',
+'surveillance_data/vehicle_data/three_wheeler/'
+}
 
---print('No of Left images ',#train_images_list_l)
-table.sort(train_images_list_l, function (a, b)
-      return string.lower(a) < string.lower(b)
-    end)
+dataset = {
+    images = torch.Tensor(nImages,nImagePlanes,imgRows,imgCols),
+    labels = torch.Tensor(nImages)
+}
 
-table.sort(train_images_list_r, function (a, b)
-      return string.lower(a) < string.lower(b)
-    end)
+for i, path in ipairs(data_path) do
+    local images_list  = paths.dir(path)
+    --  create the dataset
+    for j =  1, #images_list do
+        print(j)
+        img = image.load(path .. images_list[j])
+        print('Size of image is ', img:size())
+    end
 
-table.sort(gt_train_images_list, function (a, b)
-      return string.lower(a) < string.lower(b)
-    end)
-
-print('File names Sorted')
 
 
---nImages = # gt_train_images_list-2
-if maxImages < #gt_train_images_list-2 then
-    nImages = maxImages
-else
-    nImages = #gt_train_images_list-2
+
 end
 
-nImagePlanes = 3 -- rgb
-kitti_stereo_dataset = {
-image_left = torch.Tensor(nImages,nImagePlanes,imgRows,imgCols),
-image_right = torch.Tensor(nImages,nImagePlanes,imgRows,imgCols),
-image_gt_depth = torch.Tensor(nImages,1,imgRows,imgCols)}
 
 
---train_img_dataset = {}
---gt_train_img_dataset = {}
 
-i=1
 
-for j,img_name in ipairs(train_images_list_l) do
-    index,_ = string.find(img_name,'.png')
-    if index ~= nill then
-        --print(img_name)
-        if img_name:sub(index-1,index-1) == '0' then
-            print('Image stored' .. i)
-            --print(image.crop(image.load(train_data_path_l .. img_name),x1,y1,x2,y2):size())
+-- nImagePlanes = 3 -- rgb
+-- kitti_stereo_dataset = {
+-- image_left = torch.Tensor(nImages,nImagePlanes,imgRows,imgCols),
+-- image_right = torch.Tensor(nImages,nImagePlanes,imgRows,imgCols),
+-- image_gt_depth = torch.Tensor(nImages,1,imgRows,imgCols)}
 
-            --image.display(image.crop(image.load(train_data_path_l .. img_name),x1,y1,x2,y2))
-            kitti_stereo_dataset.image_left[i] = image.crop(image.load(train_data_path_l .. img_name),x1,y1,x2,y2)    
-        	--print(kitti_stereo_dataset.image_left[1]:size())
-        	--image.display(kitti_stereo_dataset.image_left[1])
-            kitti_stereo_dataset.image_right[{{i},{},{},{}}] = image.crop(image.load(train_data_path_r .. train_images_list_r[j]),x1,y1,x2,y2)    
-            kitti_stereo_dataset.image_gt_depth[{{i},{1},{},{}}] = image.crop(image.load(gt_train_path .. gt_train_images_list[i+2]),x1,y1,x2,y2)
-            i = i+1    
-        end
-    end
-    if i == maxImages+1 then
-        break
-    end
+
+-- --train_img_dataset = {}
+-- --gt_train_img_dataset = {}
+
+-- i=1
+
+-- for j,img_name in ipairs(train_images_list_l) do
+--     index,_ = string.find(img_name,'.png')
+--     if index ~= nill then
+--         --print(img_name)
+--         if img_name:sub(index-1,index-1) == '0' then
+--             print('Image stored' .. i)
+--             --print(image.crop(image.load(train_data_path_l .. img_name),x1,y1,x2,y2):size())
+
+--             --image.display(image.crop(image.load(train_data_path_l .. img_name),x1,y1,x2,y2))
+--             kitti_stereo_dataset.image_left[i] = image.crop(image.load(train_data_path_l .. img_name),x1,y1,x2,y2)    
+--         	--print(kitti_stereo_dataset.image_left[1]:size())
+--         	--image.display(kitti_stereo_dataset.image_left[1])
+--             kitti_stereo_dataset.image_right[{{i},{},{},{}}] = image.crop(image.load(train_data_path_r .. train_images_list_r[j]),x1,y1,x2,y2)    
+--             kitti_stereo_dataset.image_gt_depth[{{i},{1},{},{}}] = image.crop(image.load(gt_train_path .. gt_train_images_list[i+2]),x1,y1,x2,y2)
+--             i = i+1    
+--         end
+--     end
+--     if i == maxImages+1 then
+--         break
+--     end
     
-end
+-- end
 
 
-print('Images stored in table')
+-- print('Images stored in table')
 
---torch.save('kitti_stereo_dataset_100.dat',torch.Tensor(kitti_stereo_dataset))
-torch.save('kitti_stereo_dataset_190_cropped.dat',kitti_stereo_dataset)
-print('Tensor stored to disc.')
---image.display(train_img_dataset[3])
---image.display(gt_train_img_dataset[3])
+-- --torch.save('kitti_stereo_dataset_100.dat',torch.Tensor(kitti_stereo_dataset))
+-- torch.save('kitti_stereo_dataset_190_cropped.dat',kitti_stereo_dataset)
+-- print('Tensor stored to disc.')
+-- --image.display(train_img_dataset[3])
+-- --image.display(gt_train_img_dataset[3])
